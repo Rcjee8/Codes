688f9bc47c813f39ffb55294d170b45c7dce9362
Rcjee8 <61584362+Rcjee8@users.noreply.github.com>
2021-02-14 14:17:58 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,18 @@
+import pyttsx3
+
+engine = pyttsx3.init()
+engine.say("Hello")
+engine.runAndWait()
+
+
+while True:
+    message = input()
+    if message == "quit":
+        engine = pyttsx3.init()
+        engine.say("Good bye!")
+        engine.runAndWait()
+        break
+    else:
+        engine = pyttsx3.init()
+        engine.say(message)
+        engine.runAndWait()
